2a6668ab4e1e9e6acda259fb1ee59b4554b0d8fd
jhennyCarla <jhennycarlagarciaa@gmail.com>
2017-05-03 18:17:40 -0400

    version2

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,12 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
-
+$factory->define(App\rol::class, function ($faker) {
+    
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'nombre_rol' => $faker->name,
+        'descripcion_rol' => $faker->text($maxNbChars = 50),
+    
     ];
+        
 });
