73a0721c17fea4f884819108329b8934cff4b47f
tatianaGranados <tati_n-n@hotmail.com>
2017-05-08 23:04:45 -0400

    layout v2

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,43 +3,54 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="estilos.css">
-
-    <title>proyecto titulacion</title>
-
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="/css/estiloPropio.css" rel="stylesheet">
+    
+    <title>FCE-UMSS</title>
 </head>
+
 <body>
-    <header id="Encabezado">
-       <h1><center>Encabezado de la pagina web</center></h1> 
-        <div class="navbar-header"
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    TITULACION
-                </a>
+<!--cabecera d la pagina-->
+<header class="cabecera subhead">
+  <div class="container">
+    <h1>TITULACIÓN FCE UMSS</h1>
+     <a class="navbar-brand" href="{{ url('/') }}"><p>Facultad de ciencias economicas</p></a>
+  </div>
+</header>
+
+<!--Cuerpo-->
+<!--menu navegacion-->
+
+ <!--contenido-->
+<div class="container">
+    <!--submenu navegacion-->
+    <div class="row-fluid">
+      <div class="span3">
+        <ul>
+          <li><a href="">Inicio</a></li>
+          <li><a href="#">noticias</a></li>          
+          <li><a href="#">Ingresar al sistema</a></li>
+      </div>
+    <!--seccion a cambiar-->
+        <div class="span9">
+            <section>
+                <div class="container">
+                     @yield('contenido')
+                 </div>
+            </section>
         </div>
-    </header>
-    <nav>
-        @yield('menus')
-    </nav>
-    <nav>
-       @yield('submenus') 
-    </nav>
-   
-     <section>
-       <div class="container">
-            @yield('contenido')
-       </div>
-       
-    </section>
-     <footer>
-        <h1>mi pie de pagina</h1>
-       <p>ultad de Ciencias Económicas</p></br>
-       <p>nal calle Calama Este - Edificio Prototipo </p></br>
-        Fono: 591-4-4540261
+    </div>
+</div>
+<!--pie de pagina-->
+<footer class="footer">
+    <div class="container">
+        <p>facultad de Ciencias Económicas</p>
+        <p>Direccion calle Calama Este - Edificio Prototipo </p>
+        <p>Fono: 591-4-4540261
         Correo Electrónico: info@fce.umss.edu.bo
-        Cochabamba - Bolivia
-    </footer>
+        Cochabamba - Bolivia</p>
+    </div>
+</footer>
 </body>
 </html>
