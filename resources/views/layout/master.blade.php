562bd2b40b25773faf8483bd30e56aa52b066534
tatianaGranados <tati_n-n@hotmail.com>
2017-05-10 15:17:52 -0400

    master v2

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.css" rel="stylesheet">
     <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="/css/estiloPropio.css" rel="stylesheet">
     
@@ -12,28 +12,40 @@
 
 <body>
 <!--cabecera d la pagina-->
-<header class="cabecera subhead">
+<header class="cabecera subhead" id="overview">
   <div class="container">
     <h1>TITULACIÓN FCE UMSS</h1>
-     <a class="navbar-brand" href="{{ url('/') }}"><p>Facultad de ciencias economicas</p></a>
+     <a class="navbar-brand" href="{{ url('') }}"><p>Facultad de ciencias economicas</p></a>
   </div>
 </header>
 
 <!--Cuerpo-->
 <!--menu navegacion-->
 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="navbar navbar-nav">
+          <li><a href="">Inicio</a></li>
+          <li><a href="#">noticias</a></li>          
+          <li><a href="#">Ingresar al sistema</a></li>
+        </ul>
+  </div>
+</nav>
+
  <!--contenido-->
 <div class="container">
+   
+    <div class="row">
     <!--submenu navegacion-->
-    <div class="row-fluid">
-      <div class="span3">
+      <div class="span2">
         <ul>
           <li><a href="">Inicio</a></li>
           <li><a href="#">noticias</a></li>          
           <li><a href="#">Ingresar al sistema</a></li>
+        </ul>
       </div>
     <!--seccion a cambiar-->
-        <div class="span9">
+        <div class="span10">
             <section>
                 <div class="container">
                      @yield('contenido')
@@ -52,5 +64,9 @@
         Cochabamba - Bolivia</p>
     </div>
 </footer>
+
+<!--scripts-->
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery.js"></script>
 </body>
 </html>
