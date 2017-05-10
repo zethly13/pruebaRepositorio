f1ca63e0d3b7bc81893dbe3045f8beb7ea1fca18
tatianaGranados <tati_n-n@hotmail.com>
2017-05-10 15:54:54 -0400

    master v3

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <!-- <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
     <link href="/css/bootstrap.css" rel="stylesheet">
     <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="/css/estiloPropio.css" rel="stylesheet">
@@ -47,7 +47,7 @@
     <!--seccion a cambiar-->
         <div class="span10">
             <section>
-                <div class="container">
+                <div>
                      @yield('contenido')
                  </div>
             </section>
