bbd3b0dcf2864d948882d4c352fdacc682d91770
tatianaGranados <tati_n-n@hotmail.com>
2017-05-18 14:17:44 -0400

    logeo

--- resources/views/logeo.blade.php
+++ resources/views/logeo.blade.php
@@ -1,25 +1,16 @@
 @extends('layout.master')
 
 @section('contenido')
-	<h1>Registro de Sub-Rol</h1>
-	{{ Form::open(array('route' =>array('sub_roles.store','rol_seleccionado'), 'method' => 'POST'), array('role'=> 'form')) }}
-		{{-- ingresar token ejemplo; input type="hidden" name="_token" value="{{ csrf_token() }}" --}}
+	{{ Form::open( array('url'=> '#','role'=> 'form')) }}
+		
+			{{ Form::text('nombre_usuario', Input::old('username')); }}
+            {{ Form::label('contraseña_usuario','Contraseña') }}
+            {{ Form::password('password'); }}
+            {{ Form::label('lblRememberme', 'Recordar contraseña') }}
+            {{ Form::checkbox('rememberme', false) }}
+            {{ Form::submit('Enviar','class'=>'btn btn-primary' )) }}
 
-		<select name="rol_seleccionado">
-		<option value='-1'>Seleccione</option>	
-
-		@foreach ($rol as $roles)
-			<option value="{{$roles->id}}">{{$roles->nombre_rol}}</option>
-		@endforeach	
-		</select>
-		</br>
-		{{ Form::label('nombre_sub_rol', 'Nombre del Nuevo Sub-Rol') }}
-		{{ Form::text('nombre_sub_rol',null, array('placeholder'=> 'Introduce el nuevo Sub Rol', 'class' => 'form-control')) }}
-		</br>
-		{{ Form::label('desc_sub_rol', 'Descripcion del Sub-Rol') }}
-		{{ Form::text('desc_sub_rol',null, array('placeholder'=> 'Introduce  la Descripcion del nuevo Sub-Rol', 'class' => 'form-control')) }}
-		</br>
-		{{ Form::button('Crear Sub Rol', array('type'=> 'submit')) }}
+		{{ Form::button('Crear Sub Rol', array('type'=> 'submit','class'=>'btn btn-primary')) }}
 
 	{{ Form::close() }}
 
