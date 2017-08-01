ee37bea5b5f36d5050a0a12cd67d7981408af3a2
tatianaGranados <tati_n-n@hotmail.com>
2017-08-01 16:10:25 -0400

    edit
    estoy editando usuarios edit y list

--- resources/views/usuarios/editarUsuario.blade.php
+++ resources/views/usuarios/editarUsuario.blade.php
@@ -2,20 +2,23 @@
 @extends('layout.master')
 
 @section('contenido') 
- <div class="form-group">
-   {{ Form::open(array('route' =>array('usuarios.update', $user->id), 'method' => 'POST'), array('role'=> 'form')) }}
+ <div class="panel-heading "><center>EDICION FORMULARIO DE REGISTRO</center></div>
+    <div class="panel-body">
+      {{ Form::open(array('route' =>array('usuarios.update', $user->id), 'method' => 'POST'), array('role'=> 'form')) }}
    {{ method_field('PUT') }}
-         <label for="exampleInputName2">Documento de identidad</label>
-         <input type="text" class="form-control" id="exampleInputName2" value="{{ $user->doc_identidad }}" name="numero_identidad_usuario">
-         <label for="exampleInputName2">Tipo documento</label>
-         <select name="tipo_doc_usuario">
-              <option value='{{ $user->id_tipo_doc_identidad }}'>{{ $user->nombre_tipo_doc_identidad }}</option>  
+        
+      {{ Form::label('numero_identidad_usuario','Documento de indentidad:')}}
+      {{ Form::text('numero_identidad_usuario',$user->doc_identidad, array('placeholder'=> $user->doc_identidad, 'class' => 'form-control')) }}
+
+      {{ Form::label('tipo_doc_usuario','Tipo documento:') }}
+      <select name="tipo_doc_usuario">
+        <option value='{{ $user->id_tipo_doc_identidad }}'>{{ $user->nombre_tipo_doc_identidad }}</option>  
                 @foreach ($tipoDocId as $tipoDoc)
               <option value="{{$tipoDoc->id}}">{{$tipoDoc->nombre_tipo_doc_identidad}}</option>
                 @endforeach 
-         </select>
+      </select>
 
-         <label for="exampleInputName2">Expedido en:</label>
+         {!! Form::label('expedido_usuario','Expedido en:') !!}
          <select name="expedido_usuario">
              <option value='{{ $user->ciudad_expedido_doc }}'>{{ $user->nombre_ciudad }}</option>  
                 @foreach ($ciudad as $expedido)
@@ -23,63 +26,68 @@
                 @endforeach 
          </select>
 
-         <label for="exampleInputName2" class="col-sm-2 control-label">Apellidos</label>
+         {!! Form::label('apellido_usuario','Apellidos:') !!}
          <input type="text" class="form-control" id="exampleInputName2" name="apellido_usuario" value="{{ $user->apellidos }}">
-         <label for="exampleInputName2" class="col-sm-2 control-label">Nombres</label>
-         <input type="text" name="nombre_usuario" class="form-control" id="exampleInputName2" value="{{ $user->nombres }}"  >
+         
+         {!! Form::label('nombre_usuario','Nombres:') !!}
+         {!! form::text('nombre_usuario',$user->nombres,array('placeholder'=>$user->nombres, 'class'=>'form-control')) !!}
                 @if ($errors->has('name'))
                     <span class="help-block"><strong>{{ $errors->first('name') }}</strong></span>
                 @endif
-         <label for="inputBirthday" class="control-label">Fecha de Nacimiento</label>
-         <input name='fecha_nac_usuario' type="date" class="form-control" id="birthday" value="{{ $user->fecha_nac }}" required>
+         {!! Form::label('fecha_nac_usuario','Fecha de nacimiento:') !!}
+         {!! form::text('fecha_nac_usuario',$user->fecha_nac,array('placeholder'=>$user->fecha_nac, 'class'=>'form-control')) !!}
   
-         <p>datos de nacimiento</p>
-         <label for="exampleInputName2">pais</label>
+        {!! Form::label('pais_usuario','Pais:') !!}
          <select name="pais_usuario">
               <option value='-1'>Seleccione</option>  
                 @foreach ($pais as $paises)
               <option value="{{$paises->id}}">{{$paises->nombre_pais}}</option>
                 @endforeach 
          </select>
-         <label for="exampleInputName2">Ciudad</label>
+
+         {!! Form::label('ciudad_usuario','Ciudad:') !!}
          <select name="ciudad_usuario">
               <option value='-1'>Seleccione</option>  
                 @foreach ($ciudad as $expedido)
          <option value="{{$expedido->id}}">{{$expedido->nombre_ciudad}}</option>
                 @endforeach 
          </select>
-         <label for="exampleInputName2">provincia</label>
+
+         {!! Form::label('provincia_usuario','Provincia:') !!}
          <select name="provincia_usuario">
               <option value='{{ $user->id_provincia }}'>{{ $user->nombre_provincia }}</option>  
                 @foreach ($provincia as $provincia)
               <option value="{{$provincia->id}}">{{$provincia->nombre_provincia}}</option>
                 @endforeach 
         </select>
-        <label for="exampleInputName2">Genero</label>
+
+         {!! Form::label('sexo_usuario','Genero:') !!}
         <input type="text" class= "form-control"  id="sexoUsuario"  name="sexo_usuario" list="Sexo" value="{{ $user->sexo }}">
           <datalist id="Sexo">
             <option value="Femenino">
             <option value="Masculino">
           </datalist>
-        <label for="exampleInputName2">Estado civil</label>
+        {!! Form::label('estado_civil_usuario','Estado civil:') !!}
         <select name="estado_civil_usuario">
               <option value='{{ $user->id_estado_civil }}'>{{ $user->estado_civil }}</option>  
                 @foreach ($estado as $estado)
               <option value="{{$estado->id}}">{{$estado->estado_civil}}</option>
                 @endforeach 
         </select>
-        <label for="inputEmail3" class="col-sm-2 control-label">Correo Electronico</label>
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email_usuario">
-        <label for="inputEmail3" class="col-sm-2 control-label">Re-escribir correo electronico</label>
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-        <label for="exampleInputName2" class="col-sm-2 control-label">Telefono</label>
-        <input type="number" class="form-control" id="exampleInputName2" name="telefono_usuario">
-        <label for="exampleInputName2" class="col-sm-2 control-label">Celular</label>
-        <input type="number" class="form-control" id="exampleInputName2" name="celular_usuario">
-        <label for="exampleInputName2">Direccion</label>
-        <input type="text" class="form-control" id="exampleInputName2" placeholder="ingrese lugar de domicilio" name="direcion_usuario">
+        {{ Form::label('email_usuario','Correo electronico:') }}
+        {{ form::Email('email_usuario',null,['class'=>'form-control']) }}
+        
+        {{ Form::label('telefono_usuario','Telefono:') }}
+        {{ form::text('telefono_usuario',null,['class'=>'form-control']) }}
+        
+        {{ Form::label('celular_usuario','Celular:') }}
+        {{ form::text('celular_usuario',null,['class'=>'form-control']) }}
+        
+        {{ Form::label('direcion_usuario','Direccion:') }}
+        {{ form::text('direcion_usuario',null,['class'=>'form-control'])
 
   {{ Form::button('editar Usuario', array('type'=> 'submit')) }}
   {{ Form::close() }}
-</div>
+</div><!--cierre panel body-->
+</div><!--cierre panel default-->
 @endsection
