bbb4c2c0c62ead971b57d815bc733cd965a059dc
jhennyCarla <jhennycarlagarciaa@gmail.com>
2017-11-30 16:32:10 -0400

    cambio seeder

--- database/seeds/accesoSubRolSeeder.php
+++ database/seeds/accesoSubRolSeeder.php
@@ -11,6 +11,48 @@ class accesoSubRolSeeder extends Seeder
      */
     public function run()
     {
+    	App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '1',
+			'id_sub_rol'=>'1'
+
+         ]);
+    	App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '2',
+			'id_sub_rol'=>'1'
+
+         ]);
+    	App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '3',
+			'id_sub_rol'=>'1'
+
+         ]);
+    	App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '4',
+			'id_sub_rol'=>'1'
+
+         ]);
+    	App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '5',
+			'id_sub_rol'=>'1'
+
+         ]);
+         App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '6',
+			'id_sub_rol'=>'1'
+
+         ]);
+         App\Acceso_sub_rol::create([
+    		
+    		'id_sub_acceso'=> '7',
+			'id_sub_rol'=>'1'
+
+         ]);
         factory(App\Acceso_sub_rol::class, 20)->create();
     }
 }
--- database/seeds/usuarioAsignarSubRolSeeder.php
+++ database/seeds/usuarioAsignarSubRolSeeder.php
@@ -11,6 +11,20 @@ class usuarioAsignarSubRolSeeder extends Seeder
      */
     public function run()
     {
+    	App\Usuario_asignar_sub_rol::create([
+
+			'cod_sis'=>'201001230',
+			'fecha_inicio'=>'2000-03-10',
+			'fecha_fin'=>'2020-03-10',
+			'activo'=>'SI',
+			'id_funcion'=>'1',
+			'id_sub_rol'=>'1',
+			'id_unidad'=>'1',
+			'id_usuario'=>'1'
+
+         ]);
+
+ 
         factory(App\Usuario_asignar_sub_rol::class, 20)->create();
     }
 }
